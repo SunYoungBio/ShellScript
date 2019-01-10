122c63e2aa5dea537f4a85f65c8646929b6a581b
yang <yyang010@qq.com>
2019-01-10 09:29:19 +0800

    Update 0.MSI.sh

--- 0.MSI.sh
+++ 0.MSI.sh
@@ -1,6 +1,7 @@
 fatherpath='/datapool/Analysis/ResDev/RD03_Pan70/190107_TPNB500221AR_0096_AHGKFVBGX7'
 mydirs=`ls -l $fatherpath | cut -d ' ' -f 10 | awk '/RD03.*CF/'`			#get bamFile dir
-myMSIlist='/datapool/Analysis/PipelineDev/29.MSI/RD03_Pan70/181230_TPNB500221AR_0093_AHGLWJBGX7/MSI_H_paired/adjust_para/test_299list_i0.045/PanCancer_70_RD03_msi_299.list'
+myMSIlist='/datapool/Analysis/PipelineDev/29.MSI/RD03_Pan70\
+	/181230_TPNB500221AR_0093_AHGLWJBGX7/MSI_H_paired/adjust_para/test_299list_i0.045/PanCancer_70_RD03_msi_299.list'
 #echo $mydirs
 for i in $mydirs
 	do
