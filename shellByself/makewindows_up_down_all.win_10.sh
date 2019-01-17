3d8a135b6f219dc45c157be41f8974172273f386
yang <yyang010@qq.com>
2019-01-17 19:42:42 +0800

    Update makewindows_up_down_all.win_10.sh

--- shellByself/makewindows_up_down_all.win_10.sh
+++ shellByself/makewindows_up_down_all.win_10.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/sh
 bedtools merge -i $1 -d 1000 > merged1000.bed
 bedtools flank -i merged1000.bed -l 1000 -g ~/rice_genome/nip/makewindows/Tigr7_chromosomes_length.txt -r 0 | awk '{print$0"\tA"}' > up1k.bed
 bedtools flank -i merged1000.bed -l 0 -g ~/rice_genome/nip/makewindows/Tigr7_chromosomes_length.txt -r 1000 | awk '{print$0"\tC"}' > down1k.bed
